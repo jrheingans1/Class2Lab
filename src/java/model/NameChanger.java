0b95b9dfc44ec6f07adc5070b5c8d9561f2553a9
jrheingans1 <jrheingans1@L11103042785.wctc.msft>
2014-08-25 21:57:57 -0500

    Completed Lab 2

---
+++ src/java/model/NameChanger.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package model;
+
+/**
+ *
+ * @author jrheingans1
+ */
+public class NameChanger {
+    public String processName(String name){
+        return name.toUpperCase();
+    }
+}
